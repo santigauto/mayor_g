545c3d0b8c96648aa32519cc8c29d1d7aabc69ef
SantiGauto <santi.gauto5@gmail.com>
2020-04-06 13:20:41 -0300

    Actualizacion 6/4
    *Colaboraciones

--- lib/views/drawer_options/collab_page.dart
+++ lib/views/drawer_options/collab_page.dart
@@ -1,15 +1,73 @@
+import 'package:bordered_text/bordered_text.dart';
 import 'package:flutter/material.dart';
 import 'package:mayor_g/widgets/background_widget.dart';
 
 class CollabPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Container(
       child: Scaffold(
         appBar: AppBar(title: Text('Collab')),
         body: Stack(
           children: <Widget>[
             BackgroundWidget(),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal:20.0),
+                  child: BorderedText(
+                    strokeColor: Theme.of(context).primaryColor,
+                    child: Text(
+                      'Si desea colaborar en algo, por favor deje su propueata en la caja de comentarios',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 25,),
+                Container(
+                  width: size.width*0.9,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Colors.white.withOpacity(0.5),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+                      maxLines: 7,
+                      decoration: InputDecoration(
+                          hintText: 'Comente aquí',
+                          ),
+                    ),
+                  ),
+                ),
+                SizedBox(height:25),
+                 RaisedButton(
+                  child: Container(
+                    width: 150,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text("Enviar  ", style: TextStyle(fontSize: 20)),
+                        Icon(Icons.mail_outline)
+                      ],
+                    ),
+                  ),
+                  onPressed: () {
+                  },
+                  color: Colors.green[900],
+                  textColor: Colors.white,
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                  splashColor: Colors.grey,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(18.0),
+                    side: BorderSide(color: Colors.black),
+                  ),
+                ),
+              ],
+            )
           ],
         ),
       ),
--- lib/views/drawer_options/ranking_page.dart
+++ lib/views/drawer_options/ranking_page.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
+
 import 'package:mayor_g/widgets/background_widget.dart';
 
 class RankingPage extends StatelessWidget {
+  final Map<String, double> dataMap = {'right': 5, 'wrong': 3};
+
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Container(
       child: Scaffold(
         appBar: AppBar(title: Text('Ranking')),
@@ -20,15 +25,31 @@ class RankingPage extends StatelessWidget {
                           image: AssetImage('assets/award@3x.png'))),
                 ),
                 Card(
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
                   color: Theme.of(context).primaryColor,
                   child: Column(
                     children: <Widget>[
-                      _cardText(text:Text('Ranking Actual',style: TextStyle(fontSize: 26,color: Colors.white))),
-                      _cardText(text:Text('22',style: TextStyle(fontSize: 26,color: Colors.white)))
+                      _cardText(
+                          text: Text('Ranking Actual',
+                              style: TextStyle(
+                                  fontSize: 26, color: Colors.white))),
+                      _cardText(
+                          text: Text('22',
+                              style:
+                                  TextStyle(fontSize: 26, color: Colors.white)))
                     ],
                   ),
-                )
+                ),
+                Container(
+                    width: size.width * 0.7,
+                    child: PieChart(
+                      colorList: [Theme.of(context).primaryColor,Colors.red],
+                      dataMap: dataMap,
+                      showChartValueLabel: false,
+                      showLegends: false,
+                      chartValueStyle: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+                    )),
               ],
             ),
           ],
@@ -38,9 +59,6 @@ class RankingPage extends StatelessWidget {
   }
 
   Widget _cardText({text: Text}) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: text
-    );
+    return Padding(padding: const EdgeInsets.all(8.0), child: text);
   }
 }
