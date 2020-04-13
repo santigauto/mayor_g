d4defd386d0684cfbd995461b035e5206b36ab89
SantiGauto <santi.gauto5@gmail.com>
2020-04-13 08:13:46 -0300

    Actualizacion 10/4
    *new match page logic (en proceso)

--- lib/views/new_match_page.dart
+++ lib/views/new_match_page.dart
@@ -2,11 +2,43 @@ import 'package:bordered_text/bordered_text.dart';
 import 'package:flutter/material.dart';
 import 'package:mayor_g/widgets/background_widget.dart';
 
-class NewMatchPage extends StatelessWidget {
+class NewMatchPage extends StatefulWidget {
   const NewMatchPage({Key key}) : super(key: key);
 
   @override
+  _NewMatchPageState createState() => _NewMatchPageState();
+}
+
+class _NewMatchPageState extends State<NewMatchPage> {
+
+  bool _modoClasico = true;
+  bool _modoDuelo = true;
+  bool _selecOponente = false;
+  bool _selecAlAzar = false;
+  bool _canPlay = false;
+
+
+  void _playAction(){
+    if(_canPlay == true){
+      setState(() {
+        Navigator.pushNamed(context, 'question');
+      });
+  }
+}
+
+  void _modoAction(bool modo, String nombre){
+    setState(() {
+    modo = !modo;
+    print(nombre + ': $modo');
+    });
+  }
+
+
+
+  @override
   Widget build(BuildContext context) {
+  Color _noSeleccionado = Theme.of(context).primaryColor.withOpacity(0.2);
+  Color _seleccionado = Theme.of(context).primaryColor;
     return Container(
       child: Scaffold(
         appBar: AppBar(
@@ -24,11 +56,11 @@ class NewMatchPage extends StatelessWidget {
                   SizedBox(
                     height: 20,
                   ),
-                  _modoDeJuego(),
+                  _modoDeJuego(_seleccionado,_noSeleccionado),
                   SizedBox(
                     height: 20,
                   ),
-                  _seleccionOponente()
+                  _seleccionOponente(_seleccionado,_noSeleccionado)
                 ],
               ),
             ),
@@ -37,6 +69,7 @@ class NewMatchPage extends StatelessWidget {
       ),
     );
   }
+
 
   Widget _initMatchButton(context) {
     return RaisedButton(
@@ -53,14 +86,12 @@ class NewMatchPage extends StatelessWidget {
           style: TextStyle(fontSize: 35, color: Colors.white),
         )),
       ),
-      onPressed: () {
-        Navigator.pushNamed(context, 'question');
-      },
+      onPressed: _playAction,
       shape: CircleBorder(),
     );
   }
 
-  Widget _modoDeJuego() {
+  Widget _modoDeJuego(Color colorSelec, Color colorNoSelec) {
     return Column(
       children: <Widget>[
         Padding(
@@ -83,8 +114,12 @@ class NewMatchPage extends StatelessWidget {
               child: Container(
                 child: Text("CLÁSICO", style: TextStyle(fontSize: 20)),
               ),
-              onPressed: () {},
-              color: Colors.green[900],
+              onPressed: () {
+                _modoClasico = true;
+                if(_modoDuelo){
+                  _modoAction(_modoDuelo,'duelo');}
+              },
+              color: (_modoClasico = true) ? colorSelec : colorNoSelec,
               textColor: Colors.white,
               padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
               splashColor: Colors.grey,
@@ -97,8 +132,13 @@ class NewMatchPage extends StatelessWidget {
               child: Container(
                 child: Text(" DUELO ", style: TextStyle(fontSize: 20)),
               ),
-              onPressed: () {},
-              color: Colors.green[900],
+              color: (_modoDuelo = true) ? colorSelec : colorNoSelec,
+              onPressed: () {
+                _modoDuelo = true;
+                if(_modoClasico){
+                  _modoAction(_modoClasico,'clasico');
+                }
+              },
               textColor: Colors.white,
               padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
               splashColor: Colors.grey,
@@ -113,7 +153,7 @@ class NewMatchPage extends StatelessWidget {
     );
   }
 
-  Widget _seleccionOponente() {
+  Widget _seleccionOponente(Color colorSelec, Color colorNoSelec) {
     return Column(
       children: <Widget>[
         Padding(
@@ -136,8 +176,8 @@ class NewMatchPage extends StatelessWidget {
               child: Container(
                 child: Text("AMIGOS", style: TextStyle(fontSize: 20)),
               ),
+              color: (_modoClasico == false) ? colorSelec : colorNoSelec,
               onPressed: () {},
-              color: Colors.green[900],
               textColor: Colors.white,
               padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
               splashColor: Colors.grey,
@@ -150,8 +190,8 @@ class NewMatchPage extends StatelessWidget {
               child: Container(
                 child: Text("AL AZAR", style: TextStyle(fontSize: 20)),
               ),
+              color: (_modoClasico==false) ? colorSelec : colorNoSelec,
               onPressed: () {},
-              color: Colors.green[900],
               textColor: Colors.white,
               padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
               splashColor: Colors.grey,
@@ -166,3 +206,11 @@ class NewMatchPage extends StatelessWidget {
     );
   }
 }
+/*
+onPressed: () {
+  _modoDuelo = false
+  _selecOponente = true
+  _selecAlAzar = true
+
+},
+*/
